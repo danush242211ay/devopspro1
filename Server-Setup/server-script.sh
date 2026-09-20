e79401cde7aa44b74419b73667d430a132c0d58f
danush242211ay <danush8747@gmail.com>
2026-09-20 11:49:10 +0530

    jenkins_start

--- Server-Setup/server-script.sh
+++ Server-Setup/server-script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-sudo apt update -y
+sudo apt update
 sudo apt install fontconfig openjdk-21-jre -y
+java -version
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y 
+sudo apt update -y
+sudo apt install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
-sudo apt-get update -y
+sudo apt update -y
 
 # Install Docker
-sudo apt-get install docker.io -y
+sudo apt install docker.io -y
 
 # Add the 'ubuntu' and 'jenkins' users to the 'docker' group to allow running Docker without sudo
 sudo usermod -aG docker ubuntu 
